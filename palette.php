e4bf02d79e493118e328611956e5b66126585b6f
Nate Bol <ntboland@gmail.com>
2020-10-29 08:56:15 -0400

    added palette page

---
+++ palette.php
@@ -0,0 +1,248 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Block Palette #1234 - Block Palettes</title>
+  </head>
+  <body>
+    <!-- Nav -->
+    <div class="topbar">
+      <div class="container">
+        <div class="topbarText">
+          A Message For All To Hear
+        </div> 
+      </div>
+    </div>
+    <div class="custom-header" id="#">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="img/logotest.png" class="logo-size">
+                </a>
+                <button class="navbar-toggler custom-toggler" id="hamburger" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+                    <img src="images/hamburger-solid.svg" width="35px">
+                </button>
+                <div class="collapse navbar-collapse" id="navbarsExample05">
+                    <ul class="navbar-nav ml-auto custom-nav-text centeredContent">
+                      <li class="nav-item">
+                            <a href="popular" class="nav-link">Popular Palettes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="new" class="nav-link">New Palettes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="saved" class="nav-link" id="closeNav" >Saved Palettes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="create" class="nav-link btn btn-theme-nav">Create</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- End Nav -->
+    <div class="palettes">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-8 col-lg-12 paddingFix-large">
+                    <div style="position: relative">
+                        <div class="palette-float-large">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-12">
+                    <div class="palette-float-info">
+                        <h2 class="medium-title">Palette #1234</h2>
+                        <div class="subtext">
+                            <div class="likes half">
+                                <form>
+                                    <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 10,902</button>
+                                </form>
+                            </div>
+                            <div class="time half">
+                                1 Week Ago
+                            </div>
+                        </div>
+                        <div></div>
+                        <div class="blocks">
+                            <h4 class="small-title">Blocks Used:</h4>
+                            <ul>
+                                <li class="listText"><img src="img/1.png" class="smallBlock"> Acadia Planks</li>
+                                <li class="listText"><img src="img/2.png" class="smallBlock"> Bee Hive</li>
+                                <li class="listText"><img src="img/3.png" class="smallBlock"> Blackstone</li>
+                                <li class="listText"><img src="img/4.png" class="smallBlock"> Birch Log</li>
+                                <li class="listText"><img src="img/5.png" class="smallBlock"> Blue Concrete</li>
+                                <li class="listText"><img src="img/6.png" class="smallBlock"> Chiseld Quartz</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-sm-6 paddingFix">
+                    <div style="position: relative">
+                        <div class="palette-float">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                            <div class="subtext">
+                                <div class="likes half">
+                                    <form>
+                                        <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 0</button>
+                                    </form>
+                                </div>
+                                <div class="time half">
+                                    5 Mins Ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-sm-6 paddingFix">
+                    <div style="position: relative">
+                        <div class="palette-float">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                            <div class="subtext">
+                                <div class="likes half">
+                                    <form>
+                                        <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 0</button>
+                                    </form>
+                                </div>
+                                <div class="time half">
+                                    5 Mins Ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-sm-6  paddingFix">
+                    <div style="position: relative">
+                        <div class="palette-float">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                            <div class="subtext">
+                                <div class="likes half">
+                                    <form>
+                                        <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 0</button>
+                                    </form>
+                                </div>
+                                <div class="time half">
+                                    5 Mins Ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-sm-6 paddingFix">
+                    <div style="position: relative">
+                        <div class="palette-float">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                            <div class="subtext">
+                                <div class="likes half">
+                                    <form>
+                                        <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 0</button>
+                                    </form>
+                                </div>
+                                <div class="time half">
+                                    5 Mins Ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 col-sm-6 paddingFix">
+                    <div style="position: relative">
+                        <div class="palette-float">
+                            <img src="img/1.png" class="block">
+                            <img src="img/2.png" class="block">
+                            <img src="img/3.png" class="block">
+                            <img src="img/4.png" class="block">
+                            <img src="img/5.png" class="block">
+                            <img src="img/6.png" class="block">
+                            <div class="subtext">
+                                <div class="likes half">
+                                    <form>
+                                        <button type="submit" class="btn-like"><i class="fas fa-heart liked"></i> 0</button>
+                                    </form>
+                                </div>
+                                <div class="time half">
+                                    5 Mins Ago
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+    <script>
+    const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+      </script>
+  </body>
+</html>
